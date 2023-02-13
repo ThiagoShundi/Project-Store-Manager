7aaf57e797d4147517c963f2f83c20d10ade7d68
ThiagoShundi <shundi_hayashi@hotmail.com>
2023-02-13 19:16:21 -0300

    Requisito 1 Finalizado.

---
+++ src/controllers/product.controller.js
@@ -0,0 +1,24 @@
+const { productService } = require('../services');
+const errorMap = require('../utils/errorMap');
+
+const listProducts = async (_req, res) => {
+  const { type, message } = await productService.findAll();
+
+  if (type) return res.status(errorMap.mapError(type)).json({ message: 'Product not found' });
+
+  res.status(200).json(message);
+};
+
+const getProducts = async (req, res) => {
+  const { id } = req.params;
+  const { type, message } = await productService.findById(id);
+
+  if (type) return res.status(errorMap.mapError(type)).json({ message: 'Product not found' });
+
+  res.status(200).json(message);
+};
+
+module.exports = {
+  listProducts,
+  getProducts,
+};
